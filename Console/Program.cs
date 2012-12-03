faeb7210da229ed608fd5885b7e3c8ec0dc986cd
benjamin.lee <benjamin.lee@asolutions.com>
2012-12-03 16:06:24 -0600

    Added basic lookup of sqlite database and data sources

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Data.SQLite;
 
 namespace Console
 {
@@ -9,6 +6,23 @@ namespace Console
     {
         static void Main(string[] args)
         {
+            const string connString = "Data Source=db\\database.sqlite";
+
+            using (var conn = new SQLiteConnection(connString))
+            {
+                using (var cmd = new SQLiteCommand("SELECT * FROM CUSTOMER", conn))
+                {
+                    conn.Open();
+
+                    using (var dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            System.Console.WriteLine(dr.GetValue(0) + " " + dr.GetValue(1));
+                        }
+                    }
+                }
+            }
         }
     }
 }
